7058424633c14b653ecd73cf90563948df59f105
Rahul <Rahulrajesh2101u@gmail.com>
2023-06-28 11:36:37 +0530

    fix:improve ui

--- lib/body.dart
+++ lib/body.dart
@@ -102,6 +102,7 @@ class RecipePage extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
+            decoration: BoxDecoration(color: Colors.white),
             width: double.maxFinite,
             height: double.maxFinite,
             child: Stack(
@@ -141,9 +142,24 @@ class RecipePage extends StatelessWidget {
                   child: Container(
                     width: MediaQuery.of(context).size.width,
                     child: Align(
-                      alignment: Alignment.center,
+                      // alignment: Alignment.center,
                       child: Column(
                         children: [
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                recipe.name,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                          SizedBox(
+                            height: 40,
+                          ),
                           Text(
                             'Ingredients',
                             style: TextStyle(
